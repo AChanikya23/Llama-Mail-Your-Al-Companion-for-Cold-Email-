55a6624ef14991916cdf33efb583559e3575224c
Asthapuram Chanikya <asthapuramchanikya@gmail.com>
2025-07-24 22:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 
 # Set your Groq API key here directly
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "Ur API Key"
 
 # Load portfolio CSV once during startup
 portfolio_df = pd.read_csv("my_portfolio.csv")
